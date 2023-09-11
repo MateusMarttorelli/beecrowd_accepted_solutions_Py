7dc117d74ae7b66728fe40485f9cdb76ea9139e7
MateusMarttorelli <88115645+MateusMarttorelli@users.noreply.github.com>
2023-09-11 09:45:16 -0300

    Updating

--- bee_1043.py
+++ bee_1043.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 a, b, c = map(float, input().split())
 
 if a < b+c and b < a+c and c < a+b:
@@ -7,13 +6,3 @@ if a < b+c and b < a+c and c < a+b:
 else:
     area_trapezio = ((a+b) * c) / 2
     print(f'Area = {area_trapezio:.1f}')
-=======
-a, b, c = map(float, input().split())
-
-if a < b+c and b < a+c and c < a+b:
-    perimetro = a + b + c
-    print(f'Perimetro = {perimetro:.1f}')
-else:
-    area_trapezio = ((a+b) * c) / 2
-    print(f'Area = {area_trapezio:.1f}')
->>>>>>> bd57b8c5326bf8b105bee14a09bba34eb6cf4770
--- bee_1044.py
+++ bee_1044.py
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
 a, b = map(int, input().split())
 
 if b % a == 0 or a % b == 0:
     print('Sao Multiplos')
 else:
     print('Nao sao Multiplos')
-=======
-a, b = map(int, input().split())
-
-if b % a == 0 or a % b == 0:
-    print('Sao Multiplos')
-else:
-    print('Nao sao Multiplos')
->>>>>>> bd57b8c5326bf8b105bee14a09bba34eb6cf4770
