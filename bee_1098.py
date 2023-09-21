f27e9edbbaf4baeeac4416effc26289a440891c3
MateusMarttorelli <88115645+MateusMarttorelli@users.noreply.github.com>
2023-09-21 08:30:13 -0300

    Updating

---
+++ bee_1098.py
@@ -0,0 +1,15 @@
+i = 0
+j = 1
+
+while i <= 2:
+
+    while j <= i + 3:
+
+        if i != int(i):
+            print(f"I={i:.1f} J={j:.1f}")
+        else:
+            print(f"I={i:.0f} J={j:.0f}")
+        j += 1
+
+    i = round(i + 0.2, 1)
+    j = i + 1
