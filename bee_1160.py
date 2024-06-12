4a51b2552407205c9abc36629ba6af4548d8f485
MateusMarttorelli <88115645+MateusMarttorelli@users.noreply.github.com>
2024-06-12 18:43:30 -0300

    Updating

---
+++ bee_1160.py
@@ -0,0 +1,15 @@
+t = int(input())
+
+for i in range(t):
+    pa, pb, g1, g2 = map(float, input().split())
+    anos = 0
+
+    while pa <= pb and anos <= 100:
+        pa += int(pa * g1 / 100)
+        pb += int(pb * g2 / 100)
+        anos += 1
+
+    if anos > 100:
+        print("Mais de 1 seculo.")
+    else:
+        print(f"{anos} anos.")
