bf2a019b5cff710e541b1825655c06fc0aac43d4
MateusMarttorelli <88115645+MateusMarttorelli@users.noreply.github.com>
2024-07-16 19:48:49 -0300

    Updating

---
+++ bee_1189.py
@@ -0,0 +1,25 @@
+operacao = input()
+matriz = [[] for _ in range(12)]
+
+for i in range(12):
+    for j in range(12):
+        matriz[i].append(float(input()))
+
+soma = 0
+contador = 0
+
+for i in range(6):
+    for j in range(0, i):
+        soma += matriz[i][j]
+        contador += 1
+
+for i in range(6, 12):
+    for j in range(0, 11 - i):
+        soma += matriz[i][j]
+        contador += 1
+
+if operacao == 'S':
+    print(f"{soma:.1f}")
+elif operacao == 'M':
+    media = soma / contador
+    print(f"{media:.1f}")
