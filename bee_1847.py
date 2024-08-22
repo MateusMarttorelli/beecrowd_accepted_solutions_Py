6bf7765f054ca5e28c1a5951f51f949f0c7166b4
MateusMarttorelli <88115645+MateusMarttorelli@users.noreply.github.com>
2024-08-21 21:28:36 -0300

    Updating

---
+++ bee_1847.py
@@ -0,0 +1,26 @@
+a, b, c = map(int, input().split())
+
+if a > b <= c:
+    print(":)")
+
+elif a < b >= c:
+    print(":(")
+
+elif a < b < c and c - b < b - a:
+    print(":(")
+
+elif a < b < c and c - b >= b - a:
+    print(":)")
+
+elif a > b > c and b - c < a - b:
+    print(":)")
+
+elif a > b > c and b - c >= a - b:
+    print(":(")
+
+elif a == b < c:
+    print(":)")
+
+else:
+    print(":(")
+
